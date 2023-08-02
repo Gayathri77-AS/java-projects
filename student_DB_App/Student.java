dc0fce44103719c6acd37a73e60c6d628df97113
gayu <gayathrisaravanan191@gmail.com>
2023-08-02 13:20:20 +0530

    adding student id

--- student_DB_App/Student.java
+++ student_DB_App/Student.java
@@ -7,11 +7,11 @@ public class Student {
     private String firstname;
     private String lastname;
     private int gradeyear;
-    private int studentID;
+    private String studentID;
     private String courses;
     private int tutionBalance;
     private int costOfcourse = 600;
-
+    private static int id = 1000;
 
 
     //constructor prompt user to enter students name and year
@@ -25,11 +25,17 @@ public class Student {
         this.lastname = in.nextLine();
         System.out.println("1 - Freshman\n2 - Sophmore\n3 - Junior\n4 - Senior \nEnter student Class Level:");
         this.gradeyear = in.nextInt();
-        System.out.println(firstname+""+lastname+""+gradeyear);
-
+        in.close();
+        setStudentID();
+        System.out.println(firstname+""+lastname+" "+gradeyear+" "+studentID);
     }
 
     //generate an ID
+
+    private String setStudentID(){
+        id++;
+        return this.studentID = gradeyear + "" + id;
+    }
 
     //enroll in courses
 
