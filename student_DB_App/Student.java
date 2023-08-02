eab176c8a913a3c5a04365fe5cc0f54b26ac1f0f
gayu <gayathrisaravanan191@gmail.com>
2023-08-02 12:55:46 +0530

    adding 1st step

--- student_DB_App/Student.java
+++ student_DB_App/Student.java
@@ -1,18 +1,33 @@
 package student_DB_App;
 
+import java.util.Scanner;
+
 public class Student {
 
-    private String  firstname;
+    private String firstname;
     private String lastname;
-    private String gradeyear;
+    private int gradeyear;
     private int studentID;
     private String courses;
     private int tutionBalance;
-    private int costOfcourse + 600;
+    private int costOfcourse = 600;
 
 
 
     //constructor prompt user to enter students name and year
+
+
+    public Student(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter student First name:");
+        this.firstname = in.nextLine();
+        System.out.println("Enter student Last name:");
+        this.lastname = in.nextLine();
+        System.out.println("1 - Freshman\n2 - Sophmore\n3 - Junior\n4 - Senior \nEnter student Class Level:");
+        this.gradeyear = in.nextInt();
+        System.out.println(firstname+""+lastname+""+gradeyear);
+
+    }
 
     //generate an ID
 
--- student_DB_App/StudentDatabaseApp.java
+++ student_DB_App/StudentDatabaseApp.java
@@ -2,6 +2,6 @@ package student_DB_App;
 
 public class StudentDatabaseApp {
     public static void main(String[] args){
-        
+        Student st1 = new Student();
     }
 }
