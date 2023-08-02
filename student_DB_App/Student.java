ab4e9cb2b8178ca363f7e9fb62236d3967f985a6
gayu <gayathrisaravanan191@gmail.com>
2023-08-02 12:39:01 +0530

    adding proj1 pdf and starting proj2

---
+++ student_DB_App/Student.java
@@ -0,0 +1,26 @@
+package student_DB_App;
+
+public class Student {
+
+    private String  firstname;
+    private String lastname;
+    private String gradeyear;
+    private int studentID;
+    private String courses;
+    private int tutionBalance;
+    private int costOfcourse + 600;
+
+
+
+    //constructor prompt user to enter students name and year
+
+    //generate an ID
+
+    //enroll in courses
+
+    //view balance
+
+    //pay tuition
+
+    //show status
+}
