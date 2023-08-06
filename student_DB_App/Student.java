90e4b29d7e7ee1fde49a658dbd35c60ce1660534
gayu <gayathrisaravanan191@gmail.com>
2023-08-06 11:50:03 +0530

    trying to compile

--- student_DB_App/Student.java
+++ student_DB_App/Student.java
@@ -2,7 +2,6 @@ package student_DB_App;
 
 import java.util.Scanner;
 public class Student {
-
     private String firstname;
     private String lastname;
     private int gradeyear;
@@ -11,11 +10,7 @@ public class Student {
     private int tutionBalance = 0;
     private int costOfcourse = 600;
     private static int id = 1000;
-
-
     //constructor prompt user to enter students name and year
-
-
     public Student(){
         Scanner in = new Scanner(System.in);
         System.out.println("Enter student First name:");
@@ -29,21 +24,18 @@ public class Student {
         System.out.println(firstname+""+lastname+" "+gradeyear+" "+studentID);
         enroll();
     }
-
     //generate an ID
-
     private String setStudentID(){
         id++;
         return this.studentID = gradeyear + "" + id;
     }
-
     //enroll in courses
     public void enroll(){
     System.out.println("Enter the department \n1 for Sales \n2 for development \n3 for accounting \n0 for none \n Enter department code");
-    Scanner in = new Scanner(System.in);
-    int depChoice = in.nextInt();
+    Scanner loc = new Scanner(System.in);
+    int depChoice = loc.nextInt();
     System.out.println(depChoice);
-    in.close();
+    loc.close();
     /*if (cou != "Q"){
             courses = courses + "\n" + course;
             tutionBalance = tutionBalance + costOfcourse ;
@@ -52,9 +44,6 @@ public class Student {
         System.out.println("Tution Balance "+tutionBalance);*/
     }
     //view balance
-    
-
     //pay tuition
-
     //show status
 }
