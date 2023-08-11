70229db4d6e2f2f33fa3a58637ad486de50d17e0
gayu <gayathrisaravanan191@gmail.com>
2023-08-11 12:30:32 +0530

    changes  constructors

--- Bank_App/Bankaccountapp.java
+++ Bank_App/Bankaccountapp.java
@@ -2,7 +2,7 @@ package Bank_App;
 
 public class Bankaccountapp {
     public static void main(String[] args) {
-        checking chkacc1 = new checking("Tom");
+        checking chkacc1 = new checking("tom");
     }
     
 }
