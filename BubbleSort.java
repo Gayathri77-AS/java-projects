c66680cdfdfe0fd721d657fe608337baeafd5368
Gayathri A S <gayathrisaravanan191@gmail.com>
2024-12-03 21:54:50 +0530

    Bubble sort

---
+++ BubbleSort.java
@@ -0,0 +1,59 @@
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] myArray1 = {64, 34, 25, 12, 22, 11, 90, 5};
+        bubbleSort(myArray1);
+        System.out.println("Sorted array using bubble sort: ");
+        printArray(myArray1);
+
+        int[] myArray2 = {7, 3, 9, 12, 11};
+        optimizedBubbleSort(myArray2);
+        System.out.println("Sorted array using bubble sort (optimized): ");
+        printArray(myArray2);
+    }
+
+    // Basic Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Optimized Bubble Sort
+    public static void optimizedBubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no two elements were swapped by inner loop, then break
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    // Method to print the array
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
